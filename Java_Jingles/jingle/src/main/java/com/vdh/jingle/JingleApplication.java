805bc18f8051d1c77cf4304e29dc7375f22bb87e
julivheuvel <juli.heuvel@gmail.com>
2024-01-29 20:05:49 -0500

    updates

---
+++ Java_Jingles/jingle/src/main/java/com/vdh/jingle/JingleApplication.java
@@ -0,0 +1,13 @@
+package com.vdh.jingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingleApplication.class, args);
+	}
+
+}
---
+++ Java_Jingles/jingle/src/test/java/com/vdh/jingle/JingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdh.jingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
