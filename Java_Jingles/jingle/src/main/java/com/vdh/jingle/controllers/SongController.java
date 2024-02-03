bd92c1a85171b021ef0c542313aaf20164a539eb
julivheuvel <juli.heuvel@gmail.com>
2024-02-02 20:16:03 -0500

    updates

--- Java_Jingles/jingle/src/main/java/com/vdh/jingle/controllers/SongController.java
+++ Java_Jingles/jingle/src/main/java/com/vdh/jingle/controllers/SongController.java
@@ -1,5 +1,8 @@
 package com.vdh.jingle.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class SongController {
 
 }
--- Java_Jingles/jingle/src/main/java/com/vdh/jingle/controllers/UserController.java
+++ Java_Jingles/jingle/src/main/java/com/vdh/jingle/controllers/UserController.java
@@ -1,8 +1,22 @@
 package com.vdh.jingle.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.vdh.jingle.services.UserService;
 
 @Controller
 public class UserController {
+	
+	@Autowired
+	private UserService userServ;
+	
+	@GetMapping("/")
+	public String index() {
+		
+		return "index.jsp";
+	}
 
 }
