796cc9f9bed4c068f747a45344da3999128bf031
julivheuvel <juli.heuvel@gmail.com>
2024-02-11 15:09:35 -0500

    updates

--- Creatives_CSharp/Creatives/Controllers/ArtController.cs
+++ Creatives_CSharp/Creatives/Controllers/ArtController.cs
@@ -30,7 +30,7 @@ public class ArtController : Controller
 
 
     // ===========
-    // Importing Database
+    // Accesing Database for Manipulation
     // ===========
     private CreativesContext db;
     public ArtController(CreativesContext context)
@@ -44,11 +44,170 @@ public class ArtController : Controller
     [HttpGet("/dashboard")]
     public IActionResult Dashboard()
     {
+        if(!loggedIn || uid == null)
+        {
+            return RedirectToAction("Welcome", "User");
+        }
+
+        
+
+
+        // ========
+        // Get All Art
+        // ========
+        List<Art> allArt = db.Arts
+            .Include(a => a.Poster)
+            .ToList();
+
+
+
+        return View("Dashboard", allArt);
+    }
+
+    // ================
+    // New Art Render
+    // ================
+    [HttpGet("/art/new")]
+    public IActionResult New()
+    {
         if(!loggedIn)
+        {
+            return RedirectToAction("Welcome", "User");
+        }
+        return View("NewArt");
+    }
+
+    // ================
+    // New Art Post
+    // ================
+    [HttpPost("/art/create")]
+    public IActionResult CreateArt(Art newArt)
+    {
+        if(!loggedIn || uid == null)
         {
             return RedirectToAction("Index", "Users");
         }
 
-        return View("Dashboard");
+        
+        if(!ModelState.IsValid)
+        {
+            Console.WriteLine("======================");
+            Console.WriteLine("Invalid!!!!");
+            Console.WriteLine("======================");
+
+            return New();
+            }
+
+        newArt.UserId = (int)uid;
+
+        db.Arts.Add(newArt);
+        db.SaveChanges();
+        return RedirectToAction("Dashboard");
+    }
+
+    // ================
+    // View Art Render
+    // ================
+    [HttpGet("/art/{artId}/view")]
+    public IActionResult ViewArt(int artId)
+    {
+
+        if(!loggedIn)
+        {
+            return RedirectToAction("Welcome", "User");
+        }
+
+        // find the id
+        Art? art = db.Arts.FirstOrDefault(p => p.ArtId == artId);
+
+        // if id not found
+        if(art == null)
+        {
+            return RedirectToAction("Dashboard");
+        }
+
+        return View("ViewArt", art);
+    }
+
+    // ================
+    // Edit Art Render
+    // ================
+    [HttpGet("/art/{artId}/edit")]
+    public IActionResult EditArt(int artId)
+    {
+
+        if(!loggedIn)
+        {
+            return RedirectToAction("Welcome", "User");
+        }
+
+        // find the id
+        Art? art = db.Arts.FirstOrDefault(p => p.ArtId == artId);
+
+        // if id not found
+        if(art == null)
+        {
+            return RedirectToAction("Dashboard");
+        }
+
+        return View("EditArt", art);
+    }
+
+    // ============================================
+    // Edit Art Post
+    // ============================================
+    [HttpPost("/art/{artId}/update")]
+    public IActionResult UpdateArt(Art editedArt, int artId)
+    {
+        if(!loggedIn)
+        {
+            return RedirectToAction("Welcome", "User");
+        }   
+        
+        if(!ModelState.IsValid)
+        {   
+            return EditArt(artId);
+        }
+
+        // does the art exist?
+        Art? dbArt = db.Arts.FirstOrDefault(p => p.ArtId == artId);
+        if(dbArt == null)
+        {   
+            return RedirectToAction("Dashboard");
+        }
+
+        dbArt.Name = editedArt.Name;
+        dbArt.Description = editedArt.Description;
+        dbArt.Price = editedArt.Price;
+        dbArt.UpdatedAt = DateTime.Now;
+
+        db.Arts.Update(dbArt);
+        Console.WriteLine("====***********************=====");
+        db.SaveChanges();
+
+
+        return Dashboard();
+    }
+
+    // ============================================
+    // Delete Art
+    // ============================================
+    [HttpPost("/art/{deletedArtId}/delete")]
+    public IActionResult DeleteArt(int deletedArtId)
+    {
+        if(!loggedIn) {
+            return RedirectToAction("Welcome", "User");
+        }
+
+        Art? art = db.Arts.FirstOrDefault(a => a.ArtId == deletedArtId);
+
+        if(art != null)
+        {
+            db.Arts.Remove(art);
+            db.SaveChanges();
+        }
+
+        return Dashboard();
+
     }
 }
---
+++ Creatives_CSharp/Creatives/Migrations/20240211190155_2M.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Creatives.Migrations
+{
+    public partial class _2M : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
