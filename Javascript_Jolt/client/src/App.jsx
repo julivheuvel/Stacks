b6eefdb84d87daa4b7d18a75a3bc77699937884e
julivheuvel <juli.heuvel@gmail.com>
2024-02-13 21:58:46 -0500

    updates

--- Javascript_Jolt/client/src/App.jsx
+++ Javascript_Jolt/client/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
+import Home from "./views/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<>
+			{/* not inside of a route and therefore in components folder */}
+			<Navigation />
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+				<Routes>
+					{/* navigate => redirect */}
+					<Route path="/" element={ <Navigate to="/coffee" /> } />
+					{/* inside of a route and therefore in views folder */}
+					<Route path="/coffee" element={ <Home /> } />
+				</Routes>
+		</>
+	);
 }
 
-export default App
+export default App;
